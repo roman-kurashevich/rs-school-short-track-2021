2329912a264f52c8aff722b9d4f80bc1185b5d04
Roman Kurashevich <rkurashevich@gmail.com>
2021-04-12 16:04:06 +0800

    feat: complete 15

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,13 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const arr = email.split('');
+  const position = arr.lastIndexOf('@');
+  const arr2 = arr.slice(position + 1, arr.length);
+  return arr2.join('');
+
+  // throw new Error('Not implemented');
 }
 
 module.exports = getEmailDomain;
